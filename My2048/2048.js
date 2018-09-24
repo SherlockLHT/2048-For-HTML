79fc0b35ce444816c2f5d5de419caf145c86bee8
Sherlock_Lin <linhaitao@ifvmedic.com>
2018-09-24 17:18:03 +0800

    update

--- My2048/2048.js
+++ My2048/2048.js
@@ -1,145 +1,58 @@
-window.onload=function(){
+$(document).ready(function(){
 	initNum();
 	var random_row1 = getRandomNum(0, 3);
 	var random_column1 = getRandomNum(0, 3);
 	var id1 = getIdByRowColumn(random_row1, random_column1);
-	setCellNumber(id1, 2);
+	setNumberByID(id1, 2);
 	while(true){
 		var random_row2 = getRandomNum(0, 3);
 		var random_column2 = getRandomNum(0, 3);
 		if (random_row2 != random_row1 || random_column2 != random_column1) {
 			var id2 = getIdByRowColumn(random_row2, random_column2);
-			setCellNumber(id2, 2);
+			setNumberByID(id2, 2);
 			break;
 		}
 	}
-	
-}
 
-function setCellNumber(id, number){
-	var element = document.getElementById(id);
-	element.innerHTML = number
-	element.innerHTML = 0==number? "": number;
-}
+	$(document).keydown(function(event){
+		switch(event.keyCode){
+		case 37: //left
+			console.log("left");
+			break;
+		case 38: //up
+			console.log("up");
+			break;
+		case 39: //right
+			console.log("right");
+			break;
+		case 40: //down
+			console.log("down");
+			break;
+		default:
+			break;
+		}
+	});
 
-function getNumber(id){
-	var element = document.getElementById(id);
-	var value = element.innerHTML;
-	if (value == "") {	//空的表示0
-		return 0;
-	}
-	return value;
-}
-
-function getRandomNum(min, max){
-	var range = max - min;
-	var random = Math.random();
-	var num = min + Math.round(random * range);
-	return num;
-}
+	console.log(getIdByRowColumn(1,2));
+});
 
 function initNum(){
 	for(var row_index = 0; row_index < 4; row_index++){
 		for(var column_index = 0; column_index < 4; column_index++){
 			var id = getIdByRowColumn(row_index, column_index);
-			setCellNumber(id, 0);
+			setNumberByID(id, 0);
 		}
 	}
 }
 
-function getIdByRowColumn(row, column){
-	var id = "cell-" + row.toString() + "-" + column.toString();
-	return id;
-}
-
-function run(ids){
-	for(var start_index = 0; start_index < 4; start_index++){
-		var has_num = false;
-		for(var index = start_index + 1; index < 4; index++){
-			if(0 != getNumber(ids[index])){	//找到第一个不为0的数
-				has_num = true;
-				break;
-			}
-		}
-		if(!has_num){	//后面全是0
-			break;
-		}
-		if (getNumber(ids[start_index]) == 0) {
-			setCellNumber(ids[start_index], getNumber(ids[index]));
-			setCellNumber(ids[index], 0);
-			start_index--;
-		}
-		else if (getNumber(ids[start_index]) == getNumber(ids[index])) {
-			setCellNumber(ids[start_index], getNumber(ids[index]) * 2);
-			setCellNumber(ids[index], 0)
-		}
+function moveLeft(){
+	for(var row = 0; row < 3; row++){
+		moveLeftRow(row);
 	}
 }
 
-function addNewNum(){
-	while(true){
-		var random_row = getRandomNum(0, 3);
-		var random_column = getRandomNum(0, 3);
-		var id = getIdByRowColumn(random_row, random_column);
-		var value = getNumber(id);
-		if(value == 0){
-			break;
-		}
+function moveLeftRow(row){
+	for(var column = 1; column < 3; column++){
+		
 	}
-	setCellNumber(id, 2);
 }
-
-document.onkeydown=function(event){
-	switch(event.keyCode){
-	case 37: //left
-		for(var row = 0; row < 4; row++){
-			var ids = new Array(
-				getIdByRowColumn(row, 0),
-				getIdByRowColumn(row, 1), 
-				getIdByRowColumn(row, 2),
-				getIdByRowColumn(row, 3)
-			);
-			run(ids);
-		}
-		addNewNum();
-		break;
-	case 38: //up
-		for(var column = 0; column < 4; column++){
-			var ids = new Array(
-				getIdByRowColumn(0, column),
-				getIdByRowColumn(1, column),
-				getIdByRowColumn(2, column),
-				getIdByRowColumn(3, column)
-			);
-			run(ids);
-		}
-		addNewNum();
-		break;
-	case 39: //right
-		for(var row = 0; row < 4; row++){
-			var ids = new Array(
-				getIdByRowColumn(row, 3),
-				getIdByRowColumn(row, 2), 
-				getIdByRowColumn(row, 1),
-				getIdByRowColumn(row, 0)
-			);
-			run(ids);
-		}
-		addNewNum();
-		break;
-	case 40: //down
-		for(var column = 0; column < 4; column++){
-			var ids = new Array(
-				getIdByRowColumn(3, column),
-				getIdByRowColumn(2, column),
-				getIdByRowColumn(1, column),
-				getIdByRowColumn(0, column)
-			);
-			run(ids);
-		}
-		addNewNum();
-		break;
-	default:
-		break;
-	}
-};
---
+++ My2048/utils.js
@@ -0,0 +1,55 @@
+var allId = new Array(4, 4);
+allId[0][0] = "cell-0-0";
+allId[0][1] = "cell-0-1";
+allId[0][2] = "cell-0-2";
+allId[0][3] = "cell-0-3";
+allId[1][0] = "cell-1-0";
+allId[1][1] = "cell-1-1";
+allId[1][2] = "cell-1-2";
+allId[1][3] = "cell-1-3";
+allId[2][0] = "cell-2-0";
+allId[2][1] = "cell-2-1";
+allId[2][2] = "cell-2-2";
+allId[2][3] = "cell-2-3";
+allId[3][0] = "cell-3-0";
+allId[3][1] = "cell-3-1";
+allId[3][2] = "cell-3-2";
+allId[3][3] = "cell-3-3";
+
+
+function setNumberByID(id, number){
+	var element = document.getElementById(id);
+	element.innerHTML = number
+	element.innerHTML = 0==number? "": number;
+}
+
+function getNumberById(id){
+	var element = document.getElementById(id);
+	var value = element.innerHTML;
+	if (value == "") {	//空的表示0
+		return 0;
+	}
+	return value;
+}
+
+function getNumByRowColumn(row, column){
+	var id = getIdByRowColumn(row, column);
+	return getNumberById(id);
+}
+
+function setNumByRowColumn(row, column){
+	var id = getIdByRowColumn(row, column);
+	setNumberByID(id);
+}
+
+function getIdByRowColumn(row, column){
+	var id = allId[row][column];
+	return id;
+}
+
+function getRandomNum(min, max){
+	var range = max - min;
+	var random = Math.random();
+	var num = min + Math.round(random * range);
+	return num;
+}
